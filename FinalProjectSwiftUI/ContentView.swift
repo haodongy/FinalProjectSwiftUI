bf6831f5cdedeb2632d9b2d4bb84f1b35dea44ce
Haodong <yanghaodong22@gmail.com>
2022-04-02 14:09:46 -0400

    Initial Commit

---
+++ FinalProjectSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FinalProjectSwiftUI
+//
+//  Created by Haodong Yang on 4/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FinalProjectSwiftUI/FinalProjectSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectSwiftUIApp.swift
+//  FinalProjectSwiftUI
+//
+//  Created by Haodong Yang on 4/2/22.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
