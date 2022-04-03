2e13bcfbebb71c3abb9df0bb9427ed59fcb69834
Haodong <yanghaodong22@gmail.com>
2022-04-03 12:55:50 -0400

    Flight List

---
+++ FinalProjectSwiftUI/FilghtList/SearchResultView.swift
@@ -0,0 +1,36 @@
+//
+//  SearchResultView.swift
+//  FinalProjectSwiftUI
+//
+//  Created by Haodong Yang on 4/3/22.
+//
+
+import SwiftUI
+
+struct SearchResultView: View {
+    @State var flightList: [FlightInfo]
+    
+    var body: some View {
+        NavigationView {
+            List {
+                TicketListFooterView()
+                Section {
+                    ForEach(flightList, id: \.self) { flightInfo in
+                        NavigationLink(destination: FlightDetailView(flightInfo: flightInfo)) {
+                            FlightListRowView(flightInfo: flightInfo)
+                        }
+                    }
+                    
+                }
+            }
+            .navigationTitle("Flight List")
+        }
+        .navigationViewStyle(StackNavigationViewStyle())
+    }
+}
+
+struct SearchResultView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchResultView(flightList: sampleFlightData)
+    }
+}
