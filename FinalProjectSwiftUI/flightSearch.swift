71d848edec2994607fd654fe9f8cd3d8798ec23d
sitaowang <934415487@qq.com>
2022-04-03 12:44:01 -0400

    flightSearch UI fix

--- FinalProjectSwiftUI/flightSearch.swift
+++ FinalProjectSwiftUI/flightSearch.swift
@@ -7,35 +7,63 @@
 
 import SwiftUI
 
-struct flightSearch: View {
+struct FlightSearch: View {
     @State var departure: String = ""
     @State var destination: String = ""
     @State private var date = Date()
     var body: some View {
         VStack{
-            TextField("Flying from", text: $departure)
-                .keyboardType(.phonePad)
-                .multilineTextAlignment(.leading)
-                .accentColor(.purple)
-                .foregroundColor(.red)
-                .font(.system(size: 14))
-                .frame(height:38)
-                .padding(.horizontal, 26)
-                .textFieldStyle(.automatic)
-            TextField("Flying to", text: $destination)
-                .keyboardType(.phonePad)
-                .multilineTextAlignment(.leading)
-                .accentColor(.purple)
-                .foregroundColor(.red)
-                .font(.system(size: 14))
-                .frame(height:38)
-                .padding(.horizontal, 26)
-                .textFieldStyle(.automatic)
-            DatePicker(
-                    "Select dates",
-                    selection: $date,
-                    displayedComponents: [.date]
-                )
+            HStack{
+                Image(systemName: "location")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 24.0)
+                    .foregroundColor(.teal)
+                    .padding(.horizontal, 8.0)
+                TextField("Flying from", text: $departure)
+                    .keyboardType(.default)
+                    .multilineTextAlignment(.leading)
+                    .accentColor(.black)
+                    .foregroundColor(.black)
+                    .font(.system(size: 14))
+                    .frame(height:38)
+                    .padding(.horizontal, 26)
+                    .textFieldStyle(.automatic)
+                
+            }
+            
+            HStack{
+                Image(systemName: "location")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 24.0)
+                    .foregroundColor(.teal)
+                    .padding(.horizontal, 8.0)
+                TextField("Flying to", text: $destination)
+                    .keyboardType(.default)
+                    .multilineTextAlignment(.leading)
+                    .accentColor(.black)
+                    .foregroundColor(.black)
+                    .font(.system(size: 14))
+                    .frame(height:38)
+                    .padding(.horizontal, 26)
+                    .textFieldStyle(.automatic)
+                
+            }
+            HStack{
+                Image(systemName: "calendar")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 24.0)
+                    .foregroundColor(.teal)
+                    .padding(.horizontal, 8.0)
+                DatePicker(
+                        "Select dates",
+                        selection: $date,
+                        displayedComponents: [.date]
+                    )
+            }
+            
             Spacer()
             Button { } label: {
                 Text("search")
@@ -52,7 +80,7 @@ struct flightSearch: View {
 
 struct flightSearch_Previews: PreviewProvider {
     static var previews: some View {
-        flightSearch()
+        FlightSearch()
     }
 }
 
