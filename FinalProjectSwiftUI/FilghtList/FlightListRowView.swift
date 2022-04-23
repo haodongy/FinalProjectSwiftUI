d98f7955e82039be776ed48ee43b98063c0a993a
Haodong <yanghaodong22@gmail.com>
2022-04-23 17:08:49 -0400

    New upate 5:08

--- FinalProjectSwiftUI/FilghtList/FlightListRowView.swift
+++ FinalProjectSwiftUI/FilghtList/FlightListRowView.swift
@@ -11,6 +11,7 @@ struct FlightListRowView: View {
     
     
     let flightInfo: FlightInfo
+    @State var flightInfo1: FlightInfo1
     
     var body: some View {
         HStack {
@@ -74,9 +75,9 @@ struct TicketListDifferentView: View {
 
 struct FlightListRow_Previews: PreviewProvider {
     static var previews: some View {
-        FlightListRowView(flightInfo: sampleFlightData[0])
-            .frame(height: 80.0)
-            .padding(.horizontal)
-            .previewLayout(PreviewLayout.sizeThatFits)
+        FlightListRowView(flightInfo: sampleFlightData[0],flightInfo1: FlightInfo1(flight_date: "2022-04-22", flight_status: "scheduled", departure: departure(airport: "Shenyang",timezone: "Asia/Shanghai",iata: "SHE",icao: "ZYTX",terminal: nil,gate: nil,delay: nil,scheduled: "2022-04-22T12:30:00+00:00",estimated: "2022-04-22T12:30:00+00:00",actual: nil,estimated_runway: nil,actual_runway: nil), arrival: arrival(airport: "Shanghai Pudong International", timezone: "Asia/Shanghai", iata: "PVG", icao: "ZSPD", terminal: "1", gate: nil, baggage: nil, delay: nil, scheduled: "2022-04-22T15:10:00+00:00", estimated: "2022-04-22T15:10:00+00:00", actual: nil, estimated_runway: nil, actual_runway: nil), airline: airline(name: "China Eastern Airlines", iata: "MU", icao: "CES"), flight: flight(number: "8410", iata: "MU8410", icao: "CES8410", codeshared: flightsub(airline_name: "shanghai airlines", airline_iata: "", airline_icao: "fm", flight_number: "9188", flight_iata: "fm9188", flight_icao: "csh9188")), aircraft: nil, live: nil))
+        .frame(height: 80.0)
+        .padding(.horizontal)
+        .previewLayout(PreviewLayout.sizeThatFits)
     }
 }
