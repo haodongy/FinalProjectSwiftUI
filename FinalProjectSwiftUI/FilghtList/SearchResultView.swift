756e6a842de1d33c44daaf75d079cebcbe62eead
Haodong <yanghaodong22@gmail.com>
2022-04-23 21:23:04 -0400

    update 9:23

--- FinalProjectSwiftUI/FilghtList/SearchResultView.swift
+++ FinalProjectSwiftUI/FilghtList/SearchResultView.swift
@@ -10,21 +10,32 @@ import SwiftUI
 struct SearchResultView: View {
     @State var flightList: [FlightInfo]
     @ObservedObject var flightResult: FlightDownloadManager = FlightDownloadManager()
+    @ObservedObject var flighDepDesInfo: FlightDepDesInfo = FlightDepDesInfo()
+    @Environment(\.dismiss) var dismiss
     
     var body: some View {
         NavigationView {
-            List {
-                TicketListDifferentView()
-                Section {
-                    /*
-                    ForEach(flightList, id: \.self) { flightInfo in
-                        NavigationLink(destination: FlightDetailView(flightInfo: flightInfo)) {
-                            FlightListRowView(flightInfo: flightInfo, flightInfo1: FlightInfo1(flight_date: "2022-04-22", flight_status: "scheduled", departure: departure(airport: "Shenyang",timezone: "Asia/Shanghai",iata: "SHE",icao: "ZYTX",terminal: nil,gate: nil,delay: nil,scheduled: "2022-04-22T12:30:00+00:00",estimated: "2022-04-22T12:30:00+00:00",actual: nil,estimated_runway: nil,actual_runway: nil), arrival: arrival(airport: "Shanghai Pudong International", timezone: "Asia/Shanghai", iata: "PVG", icao: "ZSPD", terminal: "1", gate: nil, baggage: nil, delay: nil, scheduled: "2022-04-22T15:10:00+00:00", estimated: "2022-04-22T15:10:00+00:00", actual: nil, estimated_runway: nil, actual_runway: nil), airline: airline(name: "China Eastern Airlines", iata: "MU", icao: "CES"), flight: flight(number: "8410", iata: "MU8410", icao: "CES8410", codeshared: flightsub(airline_name: "shanghai airlines", airline_iata: "", airline_icao: "fm", flight_number: "9188", flight_iata: "fm9188", flight_icao: "csh9188")), aircraft: nil, live: nil))
+            VStack{
+                List {
+                    TicketListDifferentView()
+                    Section {
+
+                        ForEach(flightResult.flightInfo, id: \.self){ flightInfo1 in
+                            
+                            FlightListRowView(flightInfo: sampleFlightData[0], flightInfo1: flightInfo1)
                         }
+                        .onAppear(){
+                            flightResult.getRealTimeFlight(dep: flighDepDesInfo.departureInfo.icao_code, arr: flighDepDesInfo.destinationInfo.icao_code)
+                        }
+                        
                     }
-                    */
-                    //ForEach
                 }
+                Button("Back") {
+                    dismiss()
+                }
+                .font(.title)
+                .padding()
+                .background(.white)
             }
             .navigationTitle("Flight List")
         }
@@ -34,6 +45,14 @@ struct SearchResultView: View {
 
 struct SearchResultView_Previews: PreviewProvider {
     static var previews: some View {
-        SearchResultView(flightList: sampleFlightData)
+        SearchResultView(flightList: sampleFlightData,flighDepDesInfo: FlightDepDesInfo())
     }
 }
+
+/*
+ ForEach(flightList, id: \.self) { flightInfo in
+ NavigationLink(destination: FlightDetailView(flightInfo: flightInfo)) {
+ FlightListRowView(flightInfo: flightInfo, flightInfo1: FlightInfo1(flight_date: "2022-04-22", flight_status: "scheduled", departure: departure(airport: "Shenyang",timezone: "Asia/Shanghai",iata: "SHE",icao: "ZYTX",terminal: nil,gate: nil,delay: nil,scheduled: "2022-04-22T12:30:00+00:00",estimated: "2022-04-22T12:30:00+00:00",actual: nil,estimated_runway: nil,actual_runway: nil), arrival: arrival(airport: "Shanghai Pudong International", timezone: "Asia/Shanghai", iata: "PVG", icao: "ZSPD", terminal: "1", gate: nil, baggage: nil, delay: nil, scheduled: "2022-04-22T15:10:00+00:00", estimated: "2022-04-22T15:10:00+00:00", actual: nil, estimated_runway: nil, actual_runway: nil), airline: airline(name: "China Eastern Airlines", iata: "MU", icao: "CES"), flight: flight(number: "8410", iata: "MU8410", icao: "CES8410", codeshared: flightsub(airline_name: "shanghai airlines", airline_iata: "", airline_icao: "fm", flight_number: "9188", flight_iata: "fm9188", flight_icao: "csh9188")), aircraft: nil, live: nil))
+ }
+ }
+ */
--- FinalProjectSwiftUI/FlightDetail/FlightDetailView.swift
+++ FinalProjectSwiftUI/FlightDetail/FlightDetailView.swift
@@ -10,11 +10,13 @@ import SwiftUI
 struct FlightDetailView: View {
     let flightInfo: FlightInfo
     @State private var isShowModal = false
+    @State var flightInfo1: FlightInfo1
+    @State var flightDepDesInfo: FlightDepDesInfo
     
     var body: some View {
         ScrollView {
             VStack(spacing: .zero) {
-                FlightDetailTopView(flightInfo: flightInfo)
+                FlightDetailTopView(flightInfo: flightInfo,flightInfo1: self.flightInfo1, flightDepDesInfo: self.flightDepDesInfo)
                 ZStack {
                     LREdgeCutShapeView()
                         .fill(Color(UIColor.tertiarySystemBackground), style: FillStyle(eoFill: false, antialiased: false))
@@ -34,6 +36,6 @@ struct FlightDetailView: View {
 
 struct FlightDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        FlightDetailView(flightInfo: sampleFlightData[0])
+        FlightDetailView(flightInfo: sampleFlightData[0],flightInfo1: FlightInfo1(flight_date: "2022-04-22", flight_status: "scheduled", departure: departure(airport: "Shenyang",timezone: "Asia/Shanghai",iata: "SHE",icao: "ZYTX",terminal: nil,gate: nil,delay: nil,scheduled: "2022-04-22T12:30:00+00:00",estimated: "2022-04-22T12:30:00+00:00",actual: nil,estimated_runway: nil,actual_runway: nil), arrival: arrival(airport: "Shanghai Pudong International", timezone: "Asia/Shanghai", iata: "PVG", icao: "ZSPD", terminal: "1", gate: nil, baggage: nil, delay: nil, scheduled: "2022-04-22T15:10:00+00:00", estimated: "2022-04-22T15:10:00+00:00", actual: nil, estimated_runway: nil, actual_runway: nil), airline: airline(name: "China Eastern Airlines", iata: "MU", icao: "CES"), flight: flight(number: "8410", iata: "MU8410", icao: "CES8410", codeshared: flightsub(airline_name: "shanghai airlines", airline_iata: "", airline_icao: "fm", flight_number: "9188", flight_iata: "fm9188", flight_icao: "csh9188")), aircraft: nil, live: nil), flightDepDesInfo: FlightDepDesInfo())
     }
 }
