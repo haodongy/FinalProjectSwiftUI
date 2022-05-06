e74bac9c3843afc40c156c8b7faf75e39744622d
sitaowang <934415487@qq.com>
2022-05-06 11:40:40 -0400

    flightDate

---
+++ FinalProjectSwiftUI/Model/FlightDate.swift
@@ -0,0 +1,12 @@
+//
+//  FlightDepDesInfo.swift
+//  FinalProjectSwiftUI
+//
+//  Created by Haodong Yang on 4/9/22.
+//
+
+import Foundation
+
+class FlightDate: ObservableObject{
+    @Published var dateString: String = ""
+}
